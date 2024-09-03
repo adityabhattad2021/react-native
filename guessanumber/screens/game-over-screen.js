c05b9eac5ad6cf26c9c98bae4548ff9b13857984
Aditya Bhattad <adityabhattad18@gmail.com>
2024-09-03 12:17:53 +0530

    make applicaions compatible to potrait mode

--- guessanumber/screens/game-over-screen.js
+++ guessanumber/screens/game-over-screen.js
@@ -2,43 +2,55 @@ import {
     View,
     Image,
     Text,
-    StyleSheet
+    StyleSheet,
+    ScrollView,
+    useWindowDimensions
 } from "react-native";
 import Card from "../components/card";
 import PrimaryButton from "../components/primary-button";
 import Colors from "../constants/colors";
 import Title from "../components/title";
 
-export default function GameOverScreen({ handleRestart,correctAnswer,numberOfMoves}) {
+
+export default function GameOverScreen({ handleRestart, correctAnswer, numberOfMoves }) {
+
+    const {width,height} = useWindowDimensions();
+    const imageSize = {
+        width:height > 400 ? 300 : 200,
+        height:height > 400 ? 300 : 200,
+        borderRadius: height > 300 ? 150: 100,
+    }
 
     return (
-        <View style={styles.screen}>
-            <Title text={"Game Over"} />
-            <View
-                style={styles.imageContainer}
-            >
-                <Image
-                    style={styles.image}
-                    source={require("../assets/images/success.png")}
-                />
+        <ScrollView>
+            <View style={styles.screen}>
+                <Title text={"Game Over"} />
+                <View
+                    style={[styles.imageContainer,imageSize]}
+                >
+                    <Image
+                        style={styles.image}
+                        source={require("../assets/images/success.png")}
+                    />
+                </View>
+
+                <Text style={styles.stats}>
+                    Your computer took <Text style={styles.statsHighlight}>{numberOfMoves}</Text>
+                    {" "}moves to get to the number <Text style={styles.statsHighlight}>{correctAnswer}</Text>.
+                </Text>
+                <View style={styles.buttonParentContainer}>
+                    <Card>
+                        <View style={styles.buttonsContainer}>
+                            <PrimaryButton
+                                onPress={handleRestart}
+                            >
+                                Restart??
+                            </PrimaryButton>
+                        </View>
+                    </Card>
+                </View>
             </View>
-            
-            <Text style={styles.stats}>
-                Your computer took <Text style={styles.statsHighlight}>{numberOfMoves}</Text>
-                {" "}moves to get to the number <Text style={styles.statsHighlight}>{correctAnswer}</Text>.
-            </Text>
-            <View style={styles.buttonParentContainer}>
-                <Card>
-                    <View style={styles.buttonsContainer}>
-                        <PrimaryButton
-                            onPress={handleRestart}
-                        >
-                            Restart??
-                        </PrimaryButton>
-                    </View>
-                </Card>
-            </View>
-        </View>
+        </ScrollView>
     )
 }
 
@@ -48,41 +60,40 @@ const styles = StyleSheet.create({
         padding: 24,
         justifyContent: "center",
         alignItems: "center",
-        gap: 60
+        gap: 60,
+        paddingTop:60,
     },
     titleContainer: {
         width: "100%",
         padding: 30
     },
-    stats:{
+    stats: {
         color: Colors.accent.secondary,
         fontWeight: "bold",
         fontSize: 24,
-        textAlign:"center",
+        textAlign: "center",
         fontFamily: "Roboto",
         paddingVertical: 4,
         paddingHorizontal: 30,
     },
-    statsHighlight:{
-        color:Colors.accent.tertiary,
-        marginHorizontal:8,
-    },  
+    statsHighlight: {
+        color: Colors.accent.tertiary,
+        marginHorizontal: 8,
+    },
     buttonsContainer: {
         backgroundColor: Colors.background.tertiary,
         flexDirection: "row",
         justifyContent: "space-around",
     },
-    buttonParentContainer:{
-        width:"70%",
-        justifyContent:"center",
-        alignItems:"center",
-        marginHorizontal:"auto"
+    buttonParentContainer: {
+        width: "70%",
+        justifyContent: "center",
+        alignItems: "center",
+        marginHorizontal: "auto"
     },
     imageContainer: {
-        width: 300,
-        height: 300,
         overflow: "hidden",
-        borderRadius: 150,
+        
         borderWidth: 10,
         borderColor: Colors.foreground.tertiary,
     },
--- guessanumber/screens/game-screen.js
+++ guessanumber/screens/game-screen.js
@@ -4,7 +4,8 @@ import {
     StyleSheet,
     Alert,
     FlatList,
-    SafeAreaView
+    SafeAreaView,
+    useWindowDimensions
 } from "react-native"
 import Title from "../components/title";
 import Colors from "../constants/colors";
@@ -13,160 +14,202 @@ import Card from "../components/card";
 import PrimaryButton from "../components/primary-button";
 import { Ionicons } from '@expo/vector-icons';
 
-function generateRandomNumber(min,max,exclude){
-    const randomNumber = Math.floor(Math.random()*(max-min))+min;
-    return randomNumber === exclude ? generateRandomNumber(min,max,exclude) : randomNumber; 
+function generateRandomNumber(min, max, exclude) {
+    let randomNumber = Math.floor(Math.random() * (max - min)) + min;
+    while (randomNumber === exclude) {
+        randomNumber = Math.floor(Math.random() * (max - min)) + min;
+    }
+    return randomNumber;
 }
 
 export default function GameScreen({ correctAnswer, handleGameOver, setMoves, moves }) {
 
     const [minVal, setMinVal] = useState(0);
     const [maxVal, setMaxVal] = useState(100);
-    const [currentGuess,setCurrentGuess] = useState(()=>generateRandomNumber(1,100,correctAnswer))
+    const [currentGuess, setCurrentGuess] = useState(() => generateRandomNumber(1, 100, correctAnswer));
+    const { width, height } = useWindowDimensions();
 
-    useEffect(()=>{
-        setMoves(prevMoves=>[currentGuess,...prevMoves])
-    },[currentGuess,setMoves])
+    let controls = (
+        <Card>
+            <Text style={styles.guess}>
+                {currentGuess}
+            </Text>
+            <View style={styles.buttonsContainer}>
+                <PrimaryButton
+                    onPress={() => nextGuessHandler('lower')}
+                >
+                    <Ionicons
+                        name="remove"
+                        size={24}
+                        color={Colors.foreground.primary}
+                    />
+                </PrimaryButton>
+                <PrimaryButton
+                    onPress={() => nextGuessHandler('higher')}
+                >
+                    <Ionicons
+                        name="add"
+                        size={24}
+                        color={Colors.foreground.primary}
+                    />
+                </PrimaryButton>
+            </View>
+        </Card>
+    )
 
-    useEffect(()=>{
-        if(currentGuess===correctAnswer){
-            Alert.alert('Game Over','We hope you had fun playing this stupid game!',[
-                {
-                    text:'Okay',
-                    onPress:handleGameOver
-                }
-            ])
-        }
-    },[currentGuess,correctAnswer,handleGameOver]);
+    if (height < 400) {
+        controls = (
+            <Card>
 
-    const nextGuessHandler = useCallback((direction)=>{
-        if(direction === 'lower' && currentGuess < correctAnswer || direction ==='higher' && currentGuess>correctAnswer){
-            Alert.alert('Don\'t lie!','Don\'t lie trust us you don\'t wanna be stuck playing this stupid game...',[
-                {
-                    text:'Sorry!',
-                    style:'cancel'
-                }
-            ])
-        }
-
-        if(direction==='lower'){
-            setMaxVal(currentGuess)
-        }else{
-            setMinVal(currentGuess+1)
-        }
-
-        const newRandomNumber = generateRandomNumber(
-            direction === 'lower' ? minVal : currentGuess+1,
-            direction === 'lower' ? currentGuess : maxVal,
-            currentGuess
-        )
-        setCurrentGuess(newRandomNumber)
-    },[currentGuess,minVal,maxVal,correctAnswer])
-    
-    const guessRoundListLength = useMemo(()=>moves.length,[moves])
-
-    return (
-        <SafeAreaView style={styles.screen}>
-           <Title
-                text="Opponent's guess"
-           />
-           <Card>
-                <Text style={styles.guess}>
-                    {currentGuess}
-                </Text>
                 <View style={styles.buttonsContainer}>
                     <PrimaryButton
-                        onPress={()=>nextGuessHandler('lower')}
-                    >   
+                        onPress={() => nextGuessHandler('lower')}
+                    >
                         <Ionicons
                             name="remove"
                             size={24}
                             color={Colors.foreground.primary}
                         />
                     </PrimaryButton>
+                    <Text style={styles.guess}>
+                        {currentGuess}
+                    </Text>
                     <PrimaryButton
-                        onPress={()=>nextGuessHandler('higher')}
+                        onPress={() => nextGuessHandler('higher')}
                     >
                         <Ionicons
                             name="add"
                             size={24}
                             color={Colors.foreground.primary}
                         />
-                    </PrimaryButton>    
+                    </PrimaryButton>
                 </View>
-           </Card>
-           <View style={styles.listContainer}>
-            <Text
-                style={styles.statsTitle}
-            >   
-                Previous Guesses
-            </Text>
-            <FlatList
-                data={moves}
-                renderItem={({item,index})=>(
-                    <View style={styles.moveItem}>
-                        <Text style={styles.moveNumber}>#{guessRoundListLength-index}</Text>
-                        <Text style={styles.moveValue}>{item}</Text>
-                    </View>
-                )}
-                keyExtractor={(_,index)=>`guess-${index}`}
-                contentContainerStyle={styles.flatListContent}
+            </Card>
+        )
+    }
+
+
+    useEffect(() => {
+        setMoves(prevMoves => [currentGuess, ...prevMoves])
+    }, [currentGuess, setMoves])
+
+    useEffect(() => {
+        if (currentGuess === correctAnswer) {
+            Alert.alert('Game Over', 'We hope you had fun playing this stupid game!', [
+                {
+                    text: 'Okay',
+                    onPress: handleGameOver
+                }
+            ])
+        }
+    }, [currentGuess, correctAnswer, handleGameOver]);
+
+    const nextGuessHandler = useCallback((direction) => {
+        if (direction === 'lower' && currentGuess < correctAnswer || direction === 'higher' && currentGuess > correctAnswer) {
+            Alert.alert('Don\'t lie!', 'Don\'t lie trust us you don\'t wanna be stuck playing this stupid game...', [
+                {
+                    text: 'Sorry!',
+                    style: 'cancel'
+                }
+            ])
+        }
+
+        if (direction === 'lower') {
+            setMaxVal(currentGuess)
+        } else {
+            setMinVal(currentGuess + 1)
+        }
+
+        const newRandomNumber = generateRandomNumber(
+            direction === 'lower' ? minVal : currentGuess + 1,
+            direction === 'lower' ? currentGuess : maxVal,
+            currentGuess
+        )
+        setCurrentGuess(newRandomNumber)
+    }, [currentGuess, minVal, maxVal, correctAnswer])
+
+    const guessRoundListLength = useMemo(() => moves.length, [moves])
+
+    return (
+        <SafeAreaView style={styles.screen}>
+            <Title
+                text="Opponent's guess"
             />
-           </View>
+            {controls}
+            <View style={styles.listContainer}>
+                <Text
+                    style={styles.statsTitle}
+                >
+                    Previous Guesses
+                </Text>
+                <FlatList
+                    data={moves}
+                    renderItem={({ item, index }) => (
+                        <View style={styles.moveItem}>
+                            <Text style={styles.moveNumber}>#{guessRoundListLength - index}</Text>
+                            <Text style={styles.moveValue}>{item}</Text>
+                        </View>
+                    )}
+                    keyExtractor={(_, index) => `guess-${index}`}
+                    contentContainerStyle={styles.flatListContent}
+                />
+            </View>
         </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
     screen: {
-        marginTop:24,
+        marginTop: 24,
         flex: 1,
-        padding:24,
-        alignItems:'center',
-        gap:30,
+        padding: 24,
+        alignItems: 'center',
+        gap: 30,
     },
     guess: {
-        fontSize:32,
-        fontWeight:'bold',
-        color:Colors.foreground.primary,
-        textAlign:'center',
-        marginVertical:24,
+        fontSize: 32,
+        fontWeight: 'bold',
+        color: Colors.foreground.primary,
+        textAlign: 'center',
+        marginVertical: 24,
     },
     buttonsContainer: {
         flexDirection: 'row',
         justifyContent: 'space-around',
+        justifyContent: 'center',
+        gap: 6
     },
-    listContainer:{
-        flex:1,
-        padding:16,
+    listContainer: {
+        flex: 1,
+        padding: 16,
     },
-    statsTitle:{
-        fontSize:20,
-        fontWeight:"bold",
-        color:Colors.foreground.primary,
-        textAlign:'center',
-        marginVertical:16,
+    statsTitle: {
+        fontSize: 20,
+        fontWeight: "bold",
+        color: Colors.foreground.primary,
+        textAlign: 'center',
+        marginVertical: 16,
     },
-    flatListContent:{
-        flexGrow:1,
+    flatListContent: {
+        flexGrow: 1,
     },
-    moveItem:{
-        flexDirection:'row',
-        justifyContent:'space-between',
-        borderColor:Colors.background.tertiary,
-        borderWidth:1,
-        borderRadius:40,
-        paddingVertical:12,
-        paddingHorizontal:32,
-        marginVertical:8,
+    moveItem: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        borderColor: Colors.background.tertiary,
+        borderWidth: 1,
+        borderRadius: 40,
+        paddingVertical: 12,
+        paddingHorizontal: 32,
+        marginVertical: 8,
     },
-    moveNumber:{
-        fontSize:16,
-        fontWeight:"bold",
-        color:Colors.accent.secondary
+    moveNumber: {
+        fontSize: 16,
+        fontWeight: "bold",
+        color: Colors.accent.secondary
     },
-    moveValue:{
-        fontSize:18,
-        color:Colors.accent.primary
+    moveValue: {
+        fontSize: 18,
+        color: Colors.accent.primary
     }
 })
